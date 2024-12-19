41258d2d7bfbfe8ec86ff0d574ee83866bfa6c1b
kowshik <kowshikvalpireddy@gmail.com>
2024-12-19 10:13:41 +0530

    chage

--- client/vite-project/src/StudentSlice.js
+++ client/vite-project/src/StudentSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/students';
+const API_URL = 'https://task-tfsg.onrender.com/students';
 
 // Fetch all students
 export const fetchStudents = createAsyncThunk('students/fetchStudents', async () => {
